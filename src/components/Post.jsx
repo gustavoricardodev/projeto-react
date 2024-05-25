9a1bcbcef1912525dc46a7be522fabd2366c00d1
gustavo <gustavoricardodev@gmail.com>
2024-05-25 18:24:15 -0300

    feat: removing possibility of empty comments

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -14,6 +14,8 @@ export function Post({ author, publishedAt, content }) {
 
   const [newCommentText, setNewCommentText] = useState('');
 
+  console.log(newCommentText);
+
   const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
     locale: ptBR,
   });
@@ -31,7 +33,11 @@ export function Post({ author, publishedAt, content }) {
   }
 
   function handleNewCommentChange() {
+    event.target.setCustomValidity('');
     setNewCommentText(event.target.value);
+  }
+
+  function handleNewCommentInvalid () {    event.target.setCustomValidity('Esse é obrigatório!');
   }
 
   function deleteComment(commentToDelete) {
@@ -40,6 +46,8 @@ export function Post({ author, publishedAt, content }) {
 
     setComments(commentsWithoutDeletedOne);
   }
+
+  const isNewCommentEmpty = newCommentText.length === 0;
 
   return (
     <article className={styles.post}>
@@ -75,10 +83,17 @@ export function Post({ author, publishedAt, content }) {
           placeholder="Deixe um comentário"
           value={newCommentText}
           onChange={handleNewCommentChange}
+          onInvalid={handleNewCommentInvalid}
+          required
         />
 
         <footer>
-          <button type="submit">Publicar</button>
+          <button 
+            type="submit" 
+            disabled={isNewCommentEmpty}
+          >
+            Publicar
+          </button>
         </footer>
       </form>
 
